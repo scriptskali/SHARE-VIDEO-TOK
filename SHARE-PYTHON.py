e1efbd4d5e198a45eafc351cc8b6fe853224fc91
scriptskali <104281088+scriptskali@users.noreply.github.com>
2022-04-24 19:15:14 -0400

    Update and rename README.md to SHARE-PYTHON.py

---
+++ SHARE-PYTHON.py
@@ -0,0 +1,62 @@
+import requests
+import os
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+os.system("clear")
+
+print(RED+"# Top 1 Share Tik Tok")
+
+print(BLUE+"--- ------------------------")
+print("-----  |                   |")
+print(" ----  |     (•)    (•)    |")
+print("  ---  |         <>        |")
+print("   --  |                   |")
+print("       |       -_-_-_      |")
+print("       |                   |")
+print("       |___________________|")
+
+print(YELLOW+"/////////////////////")
+print("creador:s.c.r.i.p.t.s.k.a.l.i")
+print("tiktok:s.c.r.i.p.t.s.k.a.l.i")
+print("/////////////////////")
+print("                      ")
+a = int(input(RED+"1.- SHARE BRUTO\n2.-SHARE LENTO \nseleccione una opcion: "))
+
+
+if a == 1:
+    print('ponga el link del video')
+elif a == 2:    print("ponga eo link del video")
+
+import requests
+import os
+os.system("clear")                                                                                                                                                              >
+if a == 1:
+
+    a = input("SHARE: ")
+    b = 0
+    while True:
+      print((b), "|", "[+] SHARE VIDEO  >", (a))
+      b = b+1
+
+os.system("clear")
+
+import  requests
+url = input("SHARE:")
+while True:
+    r = requests.get(url)
+    r.history
+    r.status_code
+    200
+    r.url
+    (url)
+    try:
+     print(GREEN+"[+]SHARE VIDEO  ", (url))
+    except:
+        print(GREEN+"[-]ERROR SRRY")
